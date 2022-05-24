f52435f7259cf4e268380ec25abff7852f9c983b
avbalsam <avbalsam@gmail.com>
2022-05-24 18:22:22 +0300

    Add series and publications ranking

--- main.py
+++ main.py
@@ -24,6 +24,11 @@ def get_rankings(div):
     return ranked_list
 
 
+def print_ranked_list(ranked_list):
+    print("\n\n\n\n\nRanking on YU Torah by shiurim given:\n")
+    for t in range(0, len(ranked_list)):
+        print(f"{t + 1}: {ranked_list[t]['name']}; Shiur Count: {ranked_list[t]['shiur_count']}")
+
 chrome_options = webdriver.ChromeOptions()
 
 try:
@@ -42,14 +47,16 @@ driver = webdriver.Chrome(service=s, chrome_options=chrome_options)
 driver.get('http://www.yutorah.com')
 
 teachers_div = driver.find_element_by_id("navTeachersDropDown")
-locations_div = driver.find_element_by_id("navVenuesDropDown")
+series_div = driver.find_element_by_id("navSeriesDropDown")
+venues_div = driver.find_element_by_id("navVenuesDropDown")
+publications_div = driver.find_element_by_id("navPublicationsDropDown")
+
 teachers_ranked = get_rankings(teachers_div)
-locations_ranked = get_rankings(locations_div)
+series_ranked = get_rankings(series_div)
+publications_ranked = get_rankings(publications_div)
+venues_ranked = get_rankings(venues_div)
 
-print("\n\n\n\n\nRanking of all teachers on YU Torah by shiurim given:\n")
-for t in range(0, len(teachers_ranked)):
-    print(f"{t + 1}: {teachers_ranked[t]['name']}; Shiur Count: {teachers_ranked[t]['shiur_count']}")
-
-print("\n\n\n\n\nRanking of all locations on YU Torah by shiurim given:\n")
-for t in range(0, len(locations_ranked)):
-    print(f"{t + 1}: {locations_ranked[t]['name']}; Shiur Count: {locations_ranked[t]['shiur_count']}")
+print_ranked_list(teachers_ranked)
+print_ranked_list(venues_ranked)
+print_ranked_list(series_ranked)
+print_ranked_list(publications_ranked)
