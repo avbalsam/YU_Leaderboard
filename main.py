0ae2fb98cfa703bf9b57e5f3c6db9b626fc617b0
avbalsam <avbalsam@gmail.com>
2022-05-25 19:17:55 +0300

    Final touches

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
 import os
+import threading
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from flask import Flask, render_template
 
 
-def get_rankings(div):
+def run_web_app():
+    app.run()
+
+
+app = Flask(__name__)
+
+t = threading.Thread(target=run_web_app)
+t.start()
+
+
+def get_rankings(name):
+    if name == 'teachers':
+        div = driver.find_element_by_id("navTeachersDropDown")
+    elif name == 'series':
+        div = driver.find_element_by_id("navSeriesDropDown")
+    elif name == 'venues':
+        div = driver.find_element_by_id("navVenuesDropDown")
+    else:
+        return list()
     el_list = div.find_elements_by_xpath(".//a[@class='nav-item']")
 
     unordered_list = list()
     for item in el_list:
         name = item.get_attribute('innerHTML')
+        link = item.get_attribute('href')
         index = name.index("<span>")
         name = name[:index]
         shiur_count = item.find_element_by_tag_name('span')
         shiur_count = shiur_count.get_attribute('innerHTML')
         shiur_count = int(shiur_count.replace("(", "").replace(")", "").replace(",", ""))
-        unordered_list.append({'name': name.strip(), 'shiur_count': shiur_count})
-        # print(f"Name: {name.strip()}, Shiurim: {shiur_count}")
+        unordered_list.append({'name': name.strip(), 'shiur_count': shiur_count, 'link': link})
+        print(f"Name: {name.strip()}, Shiurim: {shiur_count}, Link: {link}")
 
     ranked_list = sorted(unordered_list, key=lambda teacher: teacher['shiur_count'], reverse=True)
 
@@ -28,6 +49,7 @@ def print_ranked_list(ranked_list):
     print("\n\n\n\n\nRanking on YU Torah by shiurim given:\n")
     for t in range(0, len(ranked_list)):
         print(f"{t + 1}: {ranked_list[t]['name']}; Shiur Count: {ranked_list[t]['shiur_count']}")
+
 
 chrome_options = webdriver.ChromeOptions()
 
@@ -46,17 +68,8 @@ driver = webdriver.Chrome(service=s, chrome_options=chrome_options)
 
 driver.get('http://www.yutorah.com')
 
-teachers_div = driver.find_element_by_id("navTeachersDropDown")
-series_div = driver.find_element_by_id("navSeriesDropDown")
-venues_div = driver.find_element_by_id("navVenuesDropDown")
-publications_div = driver.find_element_by_id("navPublicationsDropDown")
 
-teachers_ranked = get_rankings(teachers_div)
-series_ranked = get_rankings(series_div)
-publications_ranked = get_rankings(publications_div)
-venues_ranked = get_rankings(venues_div)
-
-print_ranked_list(teachers_ranked)
-print_ranked_list(venues_ranked)
-print_ranked_list(series_ranked)
-print_ranked_list(publications_ranked)
+@app.route("/<name>")
+def main_page(name):
+    return render_template('index.html', list_header=f"{name.capitalize()} Ranked", teachers_ranked=get_rankings(name),
+                           site_title="YULeaderboard.com")
