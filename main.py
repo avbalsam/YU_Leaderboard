07c1864a220caa671d3ee336deedd965d012dacc
avbalsam <avbalsam@gmail.com>
2022-05-26 11:56:26 +0300

    Fixed chrome options to add chromedriver path.

--- main.py
+++ main.py
@@ -45,9 +45,7 @@ def print_ranked_list(ranked_list):
 
 chrome_options = webdriver.ChromeOptions()
 
-try:
-    s = Service(ChromeDriverManager().install())
-except:
+while True:
     # setup chrome for selenium (needed for heroku builds)
     s = Service(os.environ.get("CHROMEDRIVER_PATH"))
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -56,9 +54,7 @@ except:
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
 
-driver = webdriver.Chrome(service=s, chrome_options=chrome_options)
-
-while True:
+    driver = webdriver.Chrome(service=s, chrome_options=chrome_options)
     try:
         driver.get('http://www.yutorah.com')
         break
